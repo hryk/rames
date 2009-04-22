3dc362f2018f69864c28e73eafc6ec36849b0e2f
hryk <hello@hryk.info>
2009-04-22 11:30:30 +0900

    edit config

--- config/rames.rb
+++ config/rames.rb
@@ -9,23 +9,12 @@ processor :root do
         notice "this is SPAM!!!!"
         to_processor :spam
     end
-
-    mailet :match => [ Tsukurepost ] do
-        to_processor :tsukurepost
-    end
-
 end
 
 processor :spam do
     mailet :match => [ All ] do
         to_repository 'file:///var/mail/spam'
     end
-end
-
-processor :tsukurepost do
-    mailet :match => [ Bodylength , '<=', 32 ], Tsukurepost => :body_length # Matcher::Tsukurepost::NotifyError::BodyLength
-    mailet :match => [ HasAtachment , false ] , Tsukurepost => :no_attach
-    mailet :match => [ HasTicket ]            , Tsukurepost => :create           #Tsukurepost::Create
 end
 
 processor :error do
